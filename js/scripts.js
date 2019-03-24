14643f56f801f92abd633e3a3bd8585d532201bc
Charles maina <muragemaina09@gmail.com>
2019-03-24 14:04:44 +0300

    add logical operators and control flows

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ function track() {
     var length1 = parseInt(document.getElementById("length1").value);
     var length2 = parseInt(document.getElementById("length2").value);
     var length3 = parseInt(document.getElementById("length3").value);
+    if (length1 + length2 > length3 && length1 + length3 > length2 && length3 + length2 > length1) {
+        if (length1 === length2 && length2 === length3) {
+          alert("It is an equilateral triangle");
+        }
+    
+        else if (length1 === length2 || length1 === length3 || length2 === length3) {
+          alert("It is an isosceles triangle");
+        }
