da7de71eff9c75a132b1d35adcccd2e704391d78
Charles maina <muragemaina09@gmail.com>
2019-03-24 14:03:00 +0300

    add variables to js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function track() {
+    var triangles=["equilateral","isosceles", "scalene"]
+    var length1 = parseInt(document.getElementById("length1").value);
+    var length2 = parseInt(document.getElementById("length2").value);
+    var length3 = parseInt(document.getElementById("length3").value);
